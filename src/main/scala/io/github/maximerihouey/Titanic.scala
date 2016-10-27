57cd08b9bcb54d4697b82e9b923938df397b19ab
Maxime Rihouey <maxime.rihouey@gmail.com>
2016-10-27 23:54:06 +0200

    Filling NaNs

--- src/main/scala/io/github/maximerihouey/Titanic.scala
+++ src/main/scala/io/github/maximerihouey/Titanic.scala
@@ -3,6 +3,7 @@ package io.github.maximerihouey
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
 
 /**
   * Created by maxime on 26/10/16.
@@ -19,6 +20,33 @@ object Titanic {
     val (trainDFRaw, testDFRaw) = loadData("data/train.csv", "data/test.csv", sqlContext)
     println("Train size: %s".format(trainDFRaw.count()))
     println("Test size: %s".format(testDFRaw.count()))
+
+    // Feature engineering
+    val numericFeatColNames = Seq("Age", "SibSp", "Parch", "Fare")
+    val categoricalFeatColNames = Seq("Pclass", "Sex", "Embarked")
+
+    // Filling NaNs with average for numerical values
+    val ageAvg = trainDFRaw.select("Age").union(testDFRaw.select("Age")).agg(avg("Age")).first().get(0)
+    println("Average age: %s".format(ageAvg))
+    val fareAvg = trainDFRaw.select("Fare").union(testDFRaw.select("Fare")).agg(avg("Fare")).first().get(0)
+    val fillNumNa = Map(
+      "Fare" -> ageAvg,
+      "Age" -> fareAvg
+    )
+
+    // Filling empty categorical values with most frequent value (mode)
+    val EmbarkedMode = trainDFRaw.select("Embarked").union(testDFRaw.select("Embarked"))
+                        .groupBy("Embarked")
+                        .agg(col("Embarked"), count("Embarked")).collect()
+    println("Embarked Mode")
+    println(EmbarkedMode)
+    println("Embarked Mode")
+
+    val trainDF = trainDFRaw.na.fill(fillNumNa)
+    val testDF = testDFRaw.na.fill(fillNumNa)
+
+    val ageAvg2 = trainDF.select("Age").union(testDF.select("Age")).agg(avg("Age")).first().get(0)
+    println("Average age2: %s".format(ageAvg2))
   }
 
   def loadData(trainFile: String, testFile: String, sqlContext: SQLContext): (DataFrame, DataFrame) = {
